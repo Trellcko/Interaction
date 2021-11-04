595e9d779c43eef738f501f5a32d848b0092c5c6
Trellcko <32221886+Trellcko@users.noreply.github.com>
2021-11-04 17:21:00 +0200

    2d Trigger Interacting

---
+++ Trigger2dInteracting.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Trell.Interaction
+{
+
+    [RequireComponent(typeof(Collider2D))]
+    public class Trigger2dInteracting : MonoBehaviour
+    {
+        private Interactable _interactable;
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(TryGetComponent(out _interactable))
+            {
+                _interactable.EnableInteraction();
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if(_interactable)
+            {
+                _interactable.DisableInteraction();
+                _interactable = null;
+            }
+        }
+    }
+}
